1a49ba1fe8d3811af2274823524927345db826a4
That-1-JS-Nerd <itsNathan022@gmail.com>
2022-12-20 11:52:20 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const featuresModal = document.querySelector('.features-modal');
+const companyModal = document.querySelector('.company-modal');
+const featuresNav = document.querySelector('.features');
+const companyNav = document.querySelector('.company');
+
+function showModal(element, modal) {
+    element.addEventListener('mouseover', () => {
+        modal.style.display = 'block';
+    });
+
+    element.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 400);
+    });
+}
+
+showModal(featuresNav, featuresModal);
+showModal(companyNav, companyModal);
